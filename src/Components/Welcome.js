619d2fa0e227003a6986d1e6d7f470987fb02086
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-05-28 14:02:52 -0700

    initial css welcome

---
+++ src/Components/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Welcome = (props) => {
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome">Wecome to Checklist!</h1>
+      <button className="get-started-btn">
+        <p className="get-started">GET STARTED</p>
+      </button>
+    </div>
+  );
+};
+
+export default Welcome;
