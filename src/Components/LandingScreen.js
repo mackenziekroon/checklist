f0c626799bcc748bac5a84e9b48cac8da09b9c99
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-05-28 14:50:38 -0700

    small changes

--- src/Components/LandingScreen.js
+++ src/Components/LandingScreen.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const LandingScreen = (props) => {
-  return <div>Landing Landing</div>;
+  return (
+    <div className="landing-container">
+      <div className="goals-container">
+        <h2>Reminders</h2>
+      </div>
+      <div className="daily-container">
+        <h2>Daily Goals</h2>
+        <p>Drink Water</p>
+        <input type="text" />
+        oz
+        <p>Pilates</p>
+      </div>
+      <div className="add-new-tasks">
+        <h2>Add New Goals</h2>
+      </div>
+    </div>
+  );
 };
 
 export default LandingScreen;
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Welcome = (props) => {
   return (
     <div className="welcome-container">
-      <h1 className="welcome">Wecome to Checklist!</h1>
+      <h1 className="welcome">Wecome to DailyCheck!</h1>
       <Link to="/landing">
         <button className="get-started-btn">
           <h2 className="get-started">Get Started</h2>
