410097b4a6d409207c1de48efb2c54ad340372e8
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-05-28 15:59:28 -0700

    fixed spacing landing screen

--- src/Components/LandingScreen.js
+++ src/Components/LandingScreen.js
@@ -4,17 +4,20 @@ const LandingScreen = (props) => {
   return (
     <div className="landing-container">
       <div className="goals-container">
-        <h2>Reminders</h2>
+        <h2>
+          Reminders <button className="add-btn">+</button>
+        </h2>
+        <p>View All Reminders</p>
       </div>
       <div className="daily-container">
-        <h2>Daily Goals</h2>
+        <h2>
+          Daily Goals <button className="add-btn">+</button>
+        </h2>
         <p>Drink Water</p>
         <input type="text" />
         oz
         <p>Pilates</p>
-      </div>
-      <div className="add-new-tasks">
-        <h2>Add New Goals</h2>
+        <p>View All Goals</p>
       </div>
     </div>
   );
