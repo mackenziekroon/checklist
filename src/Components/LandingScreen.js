6aadb7e7ae2228ab8a1fa7521e299f7a89f5d1db
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-05-31 12:34:51 -0700

    landing screen css redesign

--- src/Components/LandingScreen.js
+++ src/Components/LandingScreen.js
@@ -15,22 +15,23 @@ const LandingScreen = (props) => {
         </div>
       </div>
       <div className="landing-container">
-        <h1>Today is {today}</h1>
+        <h1 className="today-is">Today is {today}</h1>
         <div className="goals-container">
           <h2>
-            Reminders <button className="add-btn">+</button>
+            Reminders
+            {/* <button className="add-btn">+</button> */}
           </h2>
-          <p>View All Reminders</p>
         </div>
         <div className="daily-container">
           <h2>
-            Daily Goals <button className="add-btn">+</button>
+            Daily Goals
+            {/* <button className="add-btn">+</button> */}
           </h2>
-          <p>Drink Water</p>
+          {/* <p>Drink Water</p>
           <input type="text" />
           oz
           <p>Pilates</p>
-          <p>View All Goals</p>
+          <p>View All Goals</p> */}
         </div>
       </div>
     </div>
