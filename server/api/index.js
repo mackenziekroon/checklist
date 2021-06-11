8468df17fdff5e638811dab58db9ccaf7d404850
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-06-10 17:34:01 -0700

    added route protection

--- server/api/index.js
+++ server/api/index.js
@@ -2,24 +2,24 @@ const router = require("express").Router();
 
 module.exports = router;
 
-// function isAuthenticated(req, res, next) {
-//   if (process.env.NODE_ENV === "test" || req.user) {
-//     return next();
-//   } else {
-//     console.log("user not logged in ");
-//     const err = new Error("Unauthorized: Please Login/Signup");
-//     err.status = 401;
-//     // res.redirect('/')
-//     return next(err);
-//   }
-// }
+function isAuthenticated(req, res, next) {
+  if (process.env.NODE_ENV === "test" || req.user) {
+    return next();
+  } else {
+    console.log("user not logged in ");
+    const err = new Error("Unauthorized: Please Login/Signup");
+    err.status = 401;
+    // res.redirect('/')
+    return next(err);
+  }
+}
 
 // FOR ALL ROUTES EXCEPT Login/Signup (Which are mounted on /auth path),
 // Always check if a logged in User is accessing them.
 // If not, throw error
-// router.all("/*", isAuthenticated, async (req, res, next) => {
-//   next();
-// });
+router.all("/*", isAuthenticated, async (req, res, next) => {
+  next();
+});
 
 router.use("/users", require("./users"));
 router.use("/reminders", require("./reminders"));
